7a62047b99061a85dadbbab73c13d428f8a81a6a
Jovanni Tilborg <jovannitilborg@MacBook-Air-van-Jovanni.local>
2025-01-23 17:02:39 +0100

    Updated all selectors to match exact HTML structure

--- main.py
+++ main.py
@@ -115,10 +115,10 @@ async def get_news_with_playwright(instrument: str) -> List[dict]:
                 # Wait for page to be fully loaded
                 await page.wait_for_timeout(5000)
                 
-                # Click user icon to open login modal
+                # Click user icon (looking for the SVG path)
                 logger.info("Looking for user menu button")
-                await page.wait_for_selector('button[aria-label="Open user menu"]', timeout=60000)
-                await page.click('button[aria-label="Open user menu"]', timeout=60000)
+                await page.wait_for_selector('path[d="M17.5 9c0 1.14-.3 1.99-.79 2.54-.46.52-1.27.96-2.71.96s-2.25-.44-2.71-.96A3.74 3.74 0 0 1 10.5 9c0-1.14.3-1.99.79-2.54.46-.52 1.27-.96 2.71-.96s2.25.44 2.71.96c.5.55.79 1.4.79 2.54zM19 9c0 2.76-1.45 5-5 5s-5-2.24-5-5 1.45-5 5-5 5 2.24 5 5zm-8 8.5h6c2.04 0 3.1.5 3.76 1.1.69.63 1.11 1.55 1.5 2.8.13.42.04.95-.29 1.4-.33.46-.8.7-1.22.7H7.25c-.43 0-.89-.24-1.22-.7a1.61 1.61 0 0 1-.3-1.4 6.08 6.08 0 0 1 1.51-2.8c.65-.6 1.72-1.1 3.76-1.1zm6-1.5h-6c-4.6 0-5.88 2.33-6.7 4.96-.58 1.89.97 4.04 2.95 4.04h13.5c1.98 0 3.53-2.15 2.95-4.04C22.88 18.33 21.6 16 17 16z"]', timeout=60000)
+                await page.click('path[d="M17.5 9c0 1.14-.3 1.99-.79 2.54-.46.52-1.27.96-2.71.96s-2.25-.44-2.71-.96A3.74 3.74 0 0 1 10.5 9c0-1.14.3-1.99.79-2.54.46-.52 1.27-.96 2.71-.96s2.25.44 2.71.96c.5.55.79 1.4.79 2.54zM19 9c0 2.76-1.45 5-5 5s-5-2.24-5-5 1.45-5 5-5 5 2.24 5 5zm-8 8.5h6c2.04 0 3.1.5 3.76 1.1.69.63 1.11 1.55 1.5 2.8.13.42.04.95-.29 1.4-.33.46-.8.7-1.22.7H7.25c-.43 0-.89-.24-1.22-.7a1.61 1.61 0 0 1-.3-1.4 6.08 6.08 0 0 1 1.51-2.8c.65-.6 1.72-1.1 3.76-1.1zm6-1.5h-6c-4.6 0-5.88 2.33-6.7 4.96-.58 1.89.97 4.04 2.95 4.04h13.5c1.98 0 3.53-2.15 2.95-4.04C22.88 18.33 21.6 16 17 16z"]', timeout=60000)
                 logger.info("Clicked user menu button")
                 
                 # Wait a bit for the menu to open
@@ -155,8 +155,8 @@ async def get_news_with_playwright(instrument: str) -> List[dict]:
                 
                 # Submit form
                 logger.info("Looking for submit button")
-                await page.wait_for_selector('button[type="submit"]', timeout=60000)
-                await page.click('button[type="submit"]', timeout=60000)
+                await page.wait_for_selector('button[data-overflow-tooltip-text="Sign in"]', timeout=60000)
+                await page.click('button[data-overflow-tooltip-text="Sign in"]', timeout=60000)
                 logger.info("Clicked submit button")
                 
                 await page.wait_for_load_state('load', timeout=120000)
